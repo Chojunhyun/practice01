94c81ee771e7f381c8ffbe3c81d6787c7dc228e5
cho <you-smile@nate.com>
2016-09-06 17:19:00 +0900

    2end

--- src/practice01/Prob2.java
+++ src/practice01/Prob2.java
@@ -1,9 +1,16 @@
 package practice01;
 
-
 public class Prob2 {
 
 	public static void main(String[] args) {
+		int count = 0;
+		
+		for(int i=1; i<=8; i++) {
+			for(int j=i; j<10+i; j++) {
+				System.out.print(j + " ");
+				count++;
+			}
+			System.out.println();
+		}
 	}
-
 }
