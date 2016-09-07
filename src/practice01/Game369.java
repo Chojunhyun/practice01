2d64553ddb3b33e16de9f41196f108e5eacb77bd
cho <cho@DESKTOP-088LLCO>
2016-09-07 15:55:31 +0900

    report end

--- src/practice01/Game369.java
+++ src/practice01/Game369.java
@@ -2,6 +2,29 @@ package practice01;
 
 public class Game369 {
 	public static void main(String[] args) {
-		
+		for(int i=1; i<100; i++) {
+			String str = String.valueOf(i);
+			
+			if(str.length() == 1) {		// 한자리 수
+				if(str.charAt(0)=='3' || str.charAt(0)== '6' || str.charAt(0)=='9') {
+					System.out.println(i + " 짝");
+				}
+			}
+			else {						// 두자리 수
+				if(str.charAt(0)=='3' || str.charAt(0)== '6' || str.charAt(0)=='9'
+						|| str.charAt(1)== '3' || str.charAt(1)=='6' || str.charAt(1)=='9') {
+					System.out.print(i);
+					
+					// 두자리수 박수 구별(짝 or 짝짝)
+					if((str.charAt(0)=='3' || str.charAt(0)== '6' || str.charAt(0)=='9')
+						&& (str.charAt(1)== '3' || str.charAt(1)=='6' || str.charAt(1)=='9')) {
+						System.out.println(" 짝짝");
+					}
+					else {
+						System.out.println(" 짝");
+					}
+				}
+			}
+		}
 	}
 }
--- src/practice01/GuessNumber.java
+++ src/practice01/GuessNumber.java
@@ -1,11 +1,54 @@
 package practice01;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class GuessNumber {
 	public static void main(String[] args) {
-		
 		Scanner scanner = new Scanner( System.in );
+		
+		Random r = new Random();
+		int k = r.nextInt(100) + 1;
+		
+		int num = 0, cnt=1;		// num:입력값, cnt:카운트
+		int min=1, max=100;
+		boolean b = true;
+		
+		System.out.println("수를 결정하였습니다. 맞추어 보세요." + k);
+		System.out.println("1-100");
+		
+		while(b) {
+			System.out.print(cnt++ + " >> ");
+			num = scanner.nextInt();
+			
+			if(k == num ) {		// 정답일 경우
+				System.out.println("찾았습니다.");
+				System.out.print("다시 하시겠습니까(y/n)>>");
+				String answer = scanner.next();
+				
+				if(answer.equals("n")) {
+					b = false;				// 반복문(while) 종료
+				}
+				else {
+					k = r.nextInt(100) + 1;	// 랜덤한 수 다시 받기
+					System.out.println("수를 결정하였습니다. 맞추어 보세요." + k);
+					min = 1;
+					max = 100;
+					cnt=1;
+				}
+			}
+			else if(k > num) {		// 정답(k)보다 입력값(num)이 작을 경우 
+				System.out.println("더 높게");
+				min = num;
+				System.out.println(min + "-" + max);
+			}
+			else {					// 정답(k)보다 입력값(num) 클 경우
+				System.out.println("더 낮게");
+				max = num;
+				System.out.println(min + "-" + max);
+			}
+		}
+		
+		
 		scanner.close();
 	}
 }
